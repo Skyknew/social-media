187790a1c8bb19118750a55c3df78146755a3ca9
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-07-10 22:05:42 +0545

    Create react function and relative scss file

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import "./login.scss"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Social ChitChat</h1>
+          <p>
+          Welcome! We are overjoyed to see you. Welcome! 
+          Having you here has definitely made my day.
+          It's such a pleasure to have you here! 
+          Thank you for joining us, you are cordially welcomed! Welcome to you!
+          </p>
+          <span>Do you have an account?</span>
+          <button>Register</button>
+        </div>
+
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>Login</button>
+          </form>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
