29c8f07e0dd70c553474cef919d2628aa02f0d54
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-07-15 16:03:06 +0545

    Finish creating register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,8 +2,34 @@ import "./register.scss"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Social ChitChat</h1>
+          <p>
+          Welcome! We are overjoyed to see you. Welcome! 
+          Having you here has definitely made my day.
+          It's such a pleasure to have you here! 
+          Thank you for joining us, you are cordially welcomed! Welcome to you!
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username"/>
+            <input type="email" placeholder="Email"/>
+            <input type="password" placeholder="Password"/>
+            <input type="text" placeholder="Name"/>
+            <button>Register</button>
+          </form>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
-export default Register
+export default Register;
