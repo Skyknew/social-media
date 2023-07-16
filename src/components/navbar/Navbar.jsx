50730e97b99a4af69a1d2643ad9788f435b50f22
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-07-16 13:10:02 +0545

    Build the navbar components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from 'react-router-dom';
+import "./navbar.scss";
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration:"none" }}>
+          <span>Social ChitChat</span>
+        </Link>
+        <HomeOutlinedIcon/>
+        <DarkModeOutlinedIcon/>
+        <GridViewOutlinedIcon/>
+        <div className="search">
+          <SearchOutlinedIcon/>
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+
+      <div className="right">
+        <PersonOutlinedIcon/>
+        <EmailOutlinedIcon/>
+        <NotificationsOutlinedIcon/>
+        <div className="user">
+          <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" /> 
+          <span>SkyKnew</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
