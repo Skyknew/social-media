bcd1f1ae19662a6807f3e422a15325fdc67e1564
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-07-19 15:47:28 +0545

    imported style. scss file

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import "./style.scss";
 
 function App() {
 
@@ -18,7 +19,7 @@ function App() {
 
   const Layout = ()=> {
     return(
-      <div>
+      <div className="theme-dark">
         <Navbar />
         <div style={{display: "flex" }}>
           <LeftBar />
